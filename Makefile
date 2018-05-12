4ac5ca494b117aaa98f80c8e2f18b2e406e423a9
Rylan Dmello <rylandmello@gmail.com>
2018-05-12 08:57:22 -0400

    Starting with ch2 examples

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 
-INCLUDES := include/ch1/binary_to_decimal.hpp include/ch1/decimal_to_binary.hpp
+INCLUDES := include/ch1/binary_to_decimal.hpp include/ch1/decimal_to_binary.hpp \
+
 
 all: build/main.run
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,15 @@ int main(int argc, char** argv)
 {
   std::cout << "Hello!" << std::endl;
 
+  /*
+   * ch1: Introduction
+   *
+   * This is mainly a quick introduction to the template metaprogramming facilities
+   * in C++. We go through some compile-time decimal to binary conversion functions 
+   * which are also implemented and tested below.
+   *
+   */
+
   /* test binary_to_decimal */
   const uint64_t input = 11010;
   const uint64_t output = 26;
@@ -20,6 +29,17 @@ int main(int argc, char** argv)
   static_assert(input == rd::ch1::decimal_to_binary<output>::value, "decimal_to_binary");
   std::cout << "Convert " << output << " to binary: " 
     << rd::ch1::decimal_to_binary<output>::value << std::endl;
+
+  /*
+   * ch2: Traits and Type Manipulation
+   *
+   * Start with type-based computation using metafunctions and metadata. We implement
+   * several iterations of an iter_swap function which swaps std container iterators.
+   * Finally the chapter ends with an overview of the type traits faciltites in boost::mpl
+   *
+   */
+
+  /* test iter_swap */
 
   return 0;
 }
